a5f4f0717df99ff2c89d2396078386bbef01b42b
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-06-28 00:09:37 +0530

    complete till nomanculture
    1. created api call for product and products page
    2. Created next server for product and products api
    3. Shared a fetch products api function and currency converted function call between home and pdp application.

--- proj1/pdp/src/App.jsx
+++ proj1/pdp/src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "home/Footer";
 import Header from "home/Header";
 
 import "./index.scss";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className='text-3xl mx-auto max-w-6xl'>
-    <Header />
-    <div className='my-10'>PDP Page Content</div>
-    <Footer />
-  </div>
+  <Router>
+    <div className='text-3xl mx-auto max-w-6xl'>
+      <Header />
+      <div className='my-10'>
+        <Switch>
+          <Route path='/product/:id' component={PDPContent} />
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
